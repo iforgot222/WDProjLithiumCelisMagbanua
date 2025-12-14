8d587f63857ece9804a837806be938e655164575
iforgot222 <b2029nmmagbanua@pshs.edu.ph>
2025-12-14 20:23:27 +0800

    xommit

---
+++ public/mini-quiz/quiz.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: "What is Renaissance translated to in French?",
+        answers: [
+            { text: "A", correct: false},
+            { text: "b", correct: true},
+            { text: "c", correct: false},
+            { text: "d", correct: false},
+        ]
+    },
+    {
+        question: "question b",
+        answers: [
+            { text: "A", correct: false},
+            { text: "b", correct: true},
+            { text: "c", correct: false},
+            { text: "d", correct: false},
+        ]
+    },
+    {
+        question: "question c",
+        answers: [
+            { text: "A", correct: false},
+            { text: "b", correct: true},
+            { text: "c", correct: false},
+            { text: "d", correct: false},
+        ]
+    },
+    {
+        question: "question d",
+        answers: [
+            { text: "A", correct: false},
+            { text: "b", correct: true},
+            { text: "c", correct: false},
+            { text: "d", correct: false},
+        ]
+    },
+    {
+        question: "question e",
+        answers: [
+            { text: "A", correct: false},
+            { text: "b", correct: true},
+            { text: "c", correct: false},
+            { text: "d", correct: false},
+        ]
+    }
+]
